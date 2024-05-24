7b877c7ae29891df38c16b6929db4993c07ad14e
Nikolay Kolev <toteb@abv.bg>
2024-05-25 02:52:20 +0300

    slapd

--- roles/install-tools/files/slapd.sh
+++ roles/install-tools/files/slapd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive 
-sudo debconf-set-selections <<< 'slapd/root_password password slapdtested!'
-sudo debconf-set-selections <<< 'slapd/root_password_again password slapdtested!'
+sudo debconf-set-selections <<< 'slapd/root_password password testpassword!'
+sudo debconf-set-selections <<< 'slapd/root_password_again password testpassword!'
 sudo apt-get -y install slapd ldap-utils
