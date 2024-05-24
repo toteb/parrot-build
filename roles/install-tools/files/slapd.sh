5704070e7aac77d8e4f72ef96054d974728effbb
Nikolay Kolev <toteb@abv.bg>
2024-05-25 02:58:00 +0300

    slapd

--- roles/install-tools/files/slapd.sh
+++ roles/install-tools/files/slapd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DEBIAN_FRONTEND=noninteractive 
-sudo debconf-set-selections <<< 'slapd/root_password password testpassword!'
-sudo debconf-set-selections <<< 'slapd/root_password_again password testpassword!'
-sudo apt-get -y install slapd ldap-utils
+installnoninteractive(){
+  sudo bash -c "DEBIAN_FRONTEND=noninteractive aptitude install -q -y $*"
+}
+installnoninteractive slapd
