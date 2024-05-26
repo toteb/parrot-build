2d9f0cd7c6c1fd286eeffa56a163c61c8b15349f
Nikolay Kolev <toteb@abv.bg>
2024-05-27 01:45:51 +0300

    Main

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ user=$(whoami)
 # 1. Install Ansible
 python3 -m pip install ansible
 # 2. Add Ansible to PATH
-export PATH=$PATH:/home/$user/.local/bin
+export PATH=$PATH:/home/$(cat $user)/.local/bin
 # 3. Download kali-built repository to a specified folder
 GIT_OUTPUT_DIR="/home/$user/parrot-built"
 git clone https://github.com/toteb/parrot-build.git "$GIT_OUTPUT_DIR"
