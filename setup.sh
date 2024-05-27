00f697b1286f9107007a64ffde3995288cf3881b
Nikolay Kolev <toteb@abv.bg>
2024-05-27 11:44:13 +0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,13 @@ python3 -m pip install ansible
 export PATH=$PATH:/home/$user/.local/bin
 # 3. Download kali-built repository to a specified folder
 GIT_OUTPUT_DIR="/home/$user/parrot-build"
+if [ -d "$GIT_OUTPUT_DIR" ]; then
+  echo "Directory $DIR exists. Deleting it first"
+  rm -rf "$GIT_OUTPUT_DIR"
+  echo "Directory $GIT_OUTPUT_DIR deleted."
+else
+  echo "Directory $GIT_OUTPUT_DIR does not exist, script will continue."
+fi
 git clone https://github.com/toteb/parrot-build.git "$GIT_OUTPUT_DIR"
 # 4. Install Ansible roles
 ansible-galaxy install -r "$GIT_OUTPUT_DIR/requirements.yml"
