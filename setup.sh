f042259489dc70d0c748127bb0bb9320b9f97974
Nikolay Kolev <toteb@abv.bg>
2024-05-27 11:07:11 +0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ python3 -m pip install ansible
 # 2. Add Ansible to PATH
 export PATH=$PATH:/home/$user/.local/bin
 # 3. Download kali-built repository to a specified folder
-GIT_OUTPUT_DIR="/home/$user/parrot-built"
+GIT_OUTPUT_DIR="/home/$user/parrot-build"
 git clone https://github.com/toteb/parrot-build.git "$GIT_OUTPUT_DIR"
 # 4. Install Ansible roles
 ansible-galaxy install -r "$GIT_OUTPUT_DIR/requirements.yml"
